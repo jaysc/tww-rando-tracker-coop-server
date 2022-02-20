dd6bfe590705b628e1c9fac1bffdc1070cd715f8
Jaysc <jaysc@users.noreply.github.com>
2022-02-20 01:51:47 +0000

    set address

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ server.get("/delete/:roomId", (request, reply) => {
   reply.status(200).send();
 });
 
-server.listen(process.env.PORT, (err, address) => {
+server.listen(process.env.PORT, "0.0.0.0", (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
