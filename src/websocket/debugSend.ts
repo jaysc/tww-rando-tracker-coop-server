d7baaded7e88ff01a623d92031657193bee1847b
Jaysc <jaysc@users.noreply.github.com>
2022-11-10 19:34:00 +0000

    display name in debug

--- src/websocket/debugSend.ts
+++ src/websocket/debugSend.ts
@@ -5,9 +5,11 @@ export const DebugSend = () => {
   if (global.debugClient) {
     const debugResult = {};
 
-    const allUsers = [];
-    for (const user of global.connections.keys()) {
-      allUsers.push(user);
+    const allUsers = {}
+    for (const con of global.connections.values()) {
+      if (con.user) {
+        _.set(allUsers, con.user.id, con.user.name)
+      }
     }
     _.set(debugResult, 'allUsers', allUsers);
 
